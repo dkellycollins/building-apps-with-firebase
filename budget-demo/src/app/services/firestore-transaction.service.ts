3a9f701749e13e86eaead053a4f0bbbf457b4716
Devin Kelly-Collins <dkellycollins@gmail.com>
2021-07-12 21:13:38 -0500

    Updated security rules for firestore.

--- budget-demo/src/app/services/firestore-transaction.service.ts
+++ budget-demo/src/app/services/firestore-transaction.service.ts
@@ -3,7 +3,8 @@ import { Observable } from 'rxjs';
 import { TransactionModel } from '../models/transaction.model';
 import { AngularFirestore } from '@angular/fire/firestore';
 import firebase from 'firebase';
-import { map } from 'rxjs/operators';
+import { filter, map, switchMap, take } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -11,30 +12,38 @@ import { map } from 'rxjs/operators';
 export class FirestoreTransactionService {
 
   constructor(
-    private readonly firestore: AngularFirestore
+    private readonly firestore: AngularFirestore,
+    private readonly auth: AngularFireAuth
   ) { }
 
   public get transactions$(): Observable<Array<TransactionModel>> {
-    return this.firestore.collection<TransactionDto>('transactions')
-      .valueChanges()
-      .pipe(
-        map((collection) => {
-          return collection.map(dto => {
-            return {
-              category: dto.category,
-              amount: dto.amount,
-              date: dto.date.toDate()
-            };
-          });
-        }) 
-      );
+    return this.auth.user.pipe(
+      filter(user => !!user),
+      switchMap(user => {
+        return this.firestore.collection<TransactionDto>(
+          'transactions',
+          ref => ref.where('owner', '==', user?.uid)
+        ).valueChanges();
+      }),
+      map((collection) => {
+        return collection.map(dto => {
+          return {
+            category: dto.category,
+            amount: dto.amount,
+            date: dto.date.toDate()
+          };
+        });
+      })
+    );
   }
 
   public async add(transaction: TransactionModel): Promise<void> {
+    const user = await this.auth.user.pipe(filter(user => !!user), take(1)).toPromise();
     await this.firestore.collection<TransactionDto>('transactions').add({
       category: transaction.category,
       amount: transaction.amount,
-      date: firebase.firestore.Timestamp.fromDate(transaction.date)
+      date: firebase.firestore.Timestamp.fromDate(transaction.date),
+      owner: user?.uid!
     });
   }
 }
@@ -42,5 +51,6 @@ export class FirestoreTransactionService {
 interface TransactionDto {
   category: string;
   amount: number;
-  date: firebase.firestore.Timestamp
+  date: firebase.firestore.Timestamp;
+  owner: string;
 }
